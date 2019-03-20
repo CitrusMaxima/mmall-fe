2f9243efd45963499c22ddbad4bbfce75bc88f9a
CitrusMaxima <2663753900@qq.com>
2019-03-20 16:34:24 +0800

    安装Webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// 环境变量配置，dev/online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+console.log(WEBPACK_ENV);
+
 // 获取html-webpack-plugin参数的方法
 var getHtmlConfig = function (name) {
     return {
@@ -15,12 +19,13 @@ var getHtmlConfig = function (name) {
 // webpack config
 var config = {
     entry: {
+        'common': ['./src/page/common/index.js', 'webpack-dev-server/client?http://localhost:8088/'],
         'index': ['./src/page/index/index.js'],
-        'login': ['./src/page/login/index.js'],
-        'common': ['./src/page/common/index.js']
+        'login': ['./src/page/login/index.js']
     },
     output: {
         path: './dist',
+        publicPath: '/dist',
         filename: 'js/[name].js'
     },
     externals: {
@@ -45,5 +50,9 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('login'))
     ]
 };
+
+if ('dev' === WEBPACK_ENV){
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
+}
 
 module.exports = config;
