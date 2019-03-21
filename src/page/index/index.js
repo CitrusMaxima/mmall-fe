f8c848122321fb809036923cc2e3d0a17b372a3a
CitrusMaxima <2663753900@qq.com>
2019-03-21 16:41:28 +0800

    通用JS工具类封装-获取服务端接口url、参数

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,11 +1,3 @@
 var _mm = require('util/mm.js');
 
-_mm.request({
-    url: '/product/list.do?keyword=1',
-    success: function (res) {
-        console.log(res);
-    },
-    error: function(errMsg){
-        console.log(errMsg);
-    }
-});
+console.log(_mm.getUrlParam('test'));
--- src/util/mm.js
+++ src/util/mm.js
@@ -33,6 +33,12 @@ var _mm = {
     getServerUrl: function (path) {
         return conf.serverHost + path;
     },
+    // 获取url参数
+    getUrlParam: function (name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+        var result = window.location.search.substr(1).match(reg);
+        return result ? decodeURIComponent(result[2]) : null;
+    },
     // 统一登陆处理
     doLogin: function () {
         window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href);
