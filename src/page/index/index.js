c706fd9030e4d879f373c5679cc86883d1816c2b
CitrusMaxima <2663753900@qq.com>
2019-03-22 15:13:11 +0800

    通用导航开发(UI层)

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,7 +1,2 @@
+require('page/common/nav/index.js');
 var _mm = require('util/mm.js');
-
-var html = '<div>{{data}}</div>';
-var data = {
-    data: 'test'
-};
-console.log(_mm.renderHtml(html, data));
