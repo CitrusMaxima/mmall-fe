5fca0e51ebcd7b487248beb67c58293bb285de77
CitrusMaxima <2663753900@qq.com>
2019-03-26 13:55:16 +0800

    商品列表页的开发

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -3,6 +3,7 @@ require('page/common/nav/index.js');
 require('page/common/header/index.js');
 var _mm = require('util/mm.js');
 var _product = require('service/product-service.js');
+var Pagination = require('util/pagination/index.js');
 var templateIndex = require('./index.string');
 
 var page = {
